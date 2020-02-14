2a4d3d2cef2462302112ba9e90d4ce4e8ae7fb65
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-14 19:16:44 +0100

    feat(): Utilisation de l'app Express précédemment crée.

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // On require le module http de node
 var http = require('http');
 
-// On crée un serveur
-const server = http.createServer((req, res) => {
-  res.end('Voila la réponse du serveur');
-});
+// On importe notre app express
+const app = require('./app');
+
+// On crée un serveur en lui passant l'app express
+const server = http.createServer(app);
+
+// On définit le port de l'app
+app.set('port', process.env.PORT || 3000);
 
 // On demande au serveur d'écouter sur le port par défaut ou le 3000 si dispo
 server.listen(process.env.PORT || 3000);
