8889b273471d8e91b2e290bb8392bdea921bc2e0
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-14 19:25:46 +0100

    feat(): Découverte des middleware et de la fonction next() permettant de les enchainer

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var express = require('express');
 // Creation d'une app express
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Premier middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
 app.use((req, res) => {
   res.json({ message: 'Votre requete a bien été reçue' });
 });
