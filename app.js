a66a2c0f26e132ee027fcfa5ad0491065805bcb6
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-14 20:04:51 +0100

    feat(): Suppression des middlewares de test. Creation d'un nouveau avec la route /api/stuff. Cette route retourne un tableau d'objets JSON avec le status code 200

--- app.js
+++ app.js
@@ -4,18 +4,30 @@ var express = require('express');
 // Creation d'une app express
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Premier middleware');
-  next();
-});
+// On définit une nouvelle route
+app.use('/api/stuff', (req, res, next) => {
+  // On définit les données a retourner
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res) => {
-  res.json({ message: 'Votre requete a bien été reçue' });
+  // On set le status de la reponse et le contenu (JSON)
+  res.status(200).json(stuff);
 });
 
 // Export de cette app
