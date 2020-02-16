b425131006337fea981512df434b3c76e1cde317
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-16 23:30:13 +0100

    fix(): fix modele Thing

--- models/Thing.js
+++ models/Thing.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 // Schéma de données
 // => ID généré automatiquement
-const thingSchema = {
+const thingSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   imageUrl: { type: String, required: true },
   userId: { type: String, required: true },
   price: { type: Number, required: true },
-};
+});
 
 // On exporte le schéma en tant que modèle
 module.exports = mongoose.model('Thing', thingSchema);
