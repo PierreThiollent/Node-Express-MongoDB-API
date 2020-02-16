83dc213fd2fb3a5ce5f9e10834cefee562356c5e
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-16 22:24:56 +0100

    feat(): Modification de la route get afin de récupérer les objets à vendre en base

--- app.js
+++ app.js
@@ -42,30 +42,12 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
-// On définit une nouvelle route
-app.use('/api/stuff', (req, res, next) => {
-  // On définit les données a retourner
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-
-  // On set le status de la reponse et le contenu (JSON)
-  res.status(200).json(stuff);
+// Methode pour récupérer tous les produits en vente
+app.get('/api/stuff', (req, res, next) => {
+  // On appelle la méthode find pour récupérer tout les objets Thing en base
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
 
 // Export de cette app
