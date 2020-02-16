763ec70f9ae5bc2c0eeb93d0b9f07851589c8b4c
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-16 22:26:15 +0100

    feat(): Ajout d'une route pour modifier un objet par rapport à son id

--- app.js
+++ app.js
@@ -48,6 +48,12 @@ app.get('/api/stuff/:id', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
 // Methode pour récupérer tous les produits en vente
 app.get('/api/stuff', (req, res, next) => {
   // On appelle la méthode find pour récupérer tout les objets Thing en base
