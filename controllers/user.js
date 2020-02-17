558707bf548fd99c353906417b1b23de3eaa4030
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-17 23:30:46 +0100

    feat(): Ajout de la logique pour la route /signup

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,20 @@
 const User = require('../models/Users');
+const bcrypt = require('bcrypt');
 
-exports.signup = (req, res, next) => {};
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: 'Utilisatuer créé' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
 
 exports.login = (req, res, next) => {};
