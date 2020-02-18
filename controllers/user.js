148b98173b6c09c11c448a4e3ede63fc8ef1c678
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-18 22:41:31 +0100

    feat(): Ajout de la logique pour la route /login. Authentification ok ✅

--- controllers/user.js
+++ controllers/user.js
@@ -17,4 +17,22 @@ exports.signup = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
-exports.login = (req, res, next) => {};
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      !user
+        ? res.status(401).json({ error: 'Utilisateur non trouvé' })
+        : bcrypt
+            .compare(req.body.password, user.password)
+            .then(valid => {
+              !valid
+                ? res.status(401).json({ error: 'Mot de passe incorrect' })
+                : res.status(200).json({
+                    userId: user._id,
+                    token: 'TOKEN',
+                  });
+            })
+            .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
