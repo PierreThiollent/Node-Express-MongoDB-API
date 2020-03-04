d278e39f58511f2f1b548a080c944bc355c8ea73
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-03-04 22:38:50 +0100

    feat(): Call auth middleware on all routes to check that the request is authenticated with a valid token

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const stuffController = require('../controllers/stuff');
+const auth = require('../controllers/auth');
 
 // On crée un router
 const router = express.Router();
 
 // Midlleware pour la méthode POST
-router.post('/', stuffController.createThing);
-
-router.get('/:id', stuffController.getThing);
-
-router.put('/:id', stuffController.modifyThing);
-
-router.delete('/:id', stuffController.deleteThing);
-
+router.post('/', auth, stuffController.createThing);
+// Get by id
+router.get('/:id', auth, stuffController.getThing);
+// Modify by id
+router.put('/:id', auth, stuffController.modifyThing);
+// Delete by id
+router.delete('/:id', auth, stuffController.deleteThing);
 // Methode pour récupérer tous les produits en vente
-router.get('/', stuffController.getAllThings);
+router.get('/', auth, stuffController.getAllThings);
 
 module.exports = router;
