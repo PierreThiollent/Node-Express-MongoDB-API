6f94d14bfaf0aa1571e1d4c330b00e8f2bb7c907
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-03-11 14:08:58 +0100

    feat(): Call multer middleware on POST and PUT routes to add possibility to add image of product and modify image of product

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const stuffController = require('../controllers/stuff');
 const auth = require('../controllers/auth');
+const multer = require('../controllers/multer-config');
 
 // On crée un router
 const router = express.Router();
 
 // Midlleware pour la méthode POST
-router.post('/', auth, stuffController.createThing);
+router.post('/', auth, multer, stuffController.createThing);
 // Get by id
 router.get('/:id', auth, stuffController.getThing);
 // Modify by id
-router.put('/:id', auth, stuffController.modifyThing);
+router.put('/:id', auth, multer, stuffController.modifyThing);
 // Delete by id
 router.delete('/:id', auth, stuffController.deleteThing);
 // Methode pour récupérer tous les produits en vente
